0ff45fd0c0ecdc1fee18e36255f28abfcc5f2383
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-01-13 03:18:14 +0000

    Github PRs now showing

--- components/GithubPullRequests/GithubPullRequests.tsx
+++ components/GithubPullRequests/GithubPullRequests.tsx
@@ -1,9 +1,83 @@
-export default function GithubPullRequests() {
+import { Octokit } from "@octokit/rest"
+import Link from "next/link"
+
+type repo = {
+  owner: string
+  repo: string
+}
+
+const getBadgeColour = (prCount: number) => {
+  if (prCount === 0) return "green"
+  else if (prCount <= 5) return "amber"
+  else return "red"
+}
+
+export default async function GithubPullRequests() {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+  })
+
+  const repos: repo[] = (
+    JSON.parse(process.env.GITHUB_REPOS ?? "[]") ?? []
+  ).map((r: string) => {
+    const [owner, repo] = r.split("/")
+
+    return {
+      owner,
+      repo,
+    }
+  })
+
   return (
     <>
-      <h2 className="text-xl font-bold tracking-tight text-gray-900">
+      <h2 className="text-xl font-bold tracking-tight text-gray-900 mb-6">
         Open Pull Requests
       </h2>
+      {repos.map(async (repo: repo) => {
+        const pullRequests = await octokit.pulls.list({
+          ...repo,
+          state: "open",
+        })
+
+        const badgeColour = getBadgeColour(pullRequests.data.length)
+
+        return (
+          <section
+            key={repo.repo}
+            className="mb-4 p-4 border shadow rounded-md"
+          >
+            <div className="flex flex-1">
+              <div
+                className={`mr-4 inline-flex items-center rounded-md  p-7 text-7xl font-medium ring-1 ring-inset bg-${badgeColour}-50 text-${badgeColour}-700 ring-${badgeColour}-600/10`}
+              >
+                {pullRequests.data.length}
+              </div>
+              <div>
+                <h3 className="text-lg font-bold mb-3">
+                  {repo.owner}/{repo.repo}
+                </h3>
+                {pullRequests.data.length ? (
+                  <ul className="list-disc list-inside">
+                    {pullRequests.data.map((pr) => (
+                      <li key={pr.id}>
+                        <Link
+                          href={pr.html_url}
+                          className="underline"
+                          target="_blank"
+                        >
+                          {pr.title}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>No open PRs 🎉</p>
+                )}
+              </div>
+            </div>
+          </section>
+        )
+      })}
     </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  safelist: [
+    "bg-red-50",
+    "text-red-700",
+    "ring-red-600/10",
+    "bg-amber-50",
+    "text-amber-700",
+    "ring-amber-600/10",
+    "bg-green-50",
+    "text-green-700",
+    "ring-green-600/10",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
