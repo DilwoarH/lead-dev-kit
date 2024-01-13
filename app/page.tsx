7401274049b8b0b34e7b042858de3233719cf1bc
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-01-13 02:21:45 +0000

    Update heading styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import GithubPullRequests from "@/components/GithubPullRequests/GithubPullRequests"
+
 export default function Home() {
   return (
-    <main>
-      <h1>Lead dev board</h1>
+    <main className="p-4">
+      <div className="mb-10">
+        <p className="text-base font-semibold leading-7 text-blue-600">
+          Your favourite tools as a leader
+        </p>
+        <h1 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Lead Dev Kit
+        </h1>
+      </div>
+
+      <GithubPullRequests />
     </main>
   )
 }
---
+++ components/GithubPullRequests/GithubPullRequests.tsx
@@ -0,0 +1,9 @@
+export default function GithubPullRequests() {
+  return (
+    <>
+      <h2 className="text-xl font-bold tracking-tight text-gray-900">
+        Open Pull Requests
+      </h2>
+    </>
+  )
+}
