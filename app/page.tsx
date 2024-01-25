66498757284f42ea1ced5338fdc54c4cdff75388
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2024-01-25 23:30:54 +0000

    add page updated time

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,21 @@ export const revalidate = 60 // revalidate every minute
 export default function Home() {
   return (
     <main className="p-4 bg-gray-50">
-      <div className="mb-10">
-        <p className="text-base font-semibold leading-7 text-blue-600">
-          {process.env.COMPANY_NAME ?? "LeadDevKit"}
+      <div className="mb-10 flex justify-between">
+        <div>
+          <p className="text-base font-semibold leading-7 text-blue-600">
+            {process.env.COMPANY_NAME ?? "LeadDevKit"}
+          </p>
+          <h1 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Development Team Overview
+          </h1>
+        </div>
+        <p className="text-sm my-5 text-right">
+          Page last updated: {moment().format("ddd DD MMM YYYY hh:mm a")}
         </p>
-        <h1 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Development Team Overview
-        </h1>
       </div>
 
       <GithubPullRequests />
-      <p className="text-sm my-5 text-right">
-        Page last updated: {moment().format("ddd DD MMM YYYY hh:mm a")}
-      </p>
     </main>
   )
 }
